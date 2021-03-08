79a5ca7876005af0223bcb28ec243c7c7c93f3ab
Grant Crofton <grantac@hotmail.com>
2021-03-08 14:39:11 +0000

    Added record page

--- Habitual.Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ Habitual.Client/obj/Debug/net5.0/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,70 +13,70 @@ namespace Habitual.Client.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 1 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 2 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 3 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 4 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 5 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 6 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 7 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 8 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 9 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Habitual.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
+#line 10 "C:\Programming\Blazor\Habitual\Habitual.Client\_Imports.razor"
 using Habitual.Client.Shared;
 
 #line default
@@ -90,7 +90,7 @@ using Habitual.Client.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 28 "C:\programming\Blazor\Habitual\Habitual.Client\Shared\NavMenu.razor"
+#line 28 "C:\Programming\Blazor\Habitual\Habitual.Client\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
